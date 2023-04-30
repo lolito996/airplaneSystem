41ccff23b16dc11f8d2706bc1e3c8f4b53e96660
lolito996 <josealejandromc4@gmail.com>
2023-04-29 20:05:54 -0500

    upload interface User

--- src/model/HashComparate.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface HashComparate<K extends Comparable<K>,V> {
-    public void insert(K key, V value);
-    public V search(K key);
-    public void delete(K key);
-}
